54a3c98bbc0ffe138a25eb55c442ea694297e442
awsh10 <awsh@ukr.net>
2017-09-24 22:10:37 +0300

    delete old files

--- my_dictionary/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MyDictionaryConfig(AppConfig):
-    name = 'my_dictionary'
--- my_dictionary/views.py
+++
@@ -1,9 +0,0 @@
-from datetime import date
-
-from django.http import HttpResponse
-
-def index(request):
-    today = date.today()
-    return HttpResponse("Hello, word! - " + str(date.today()))
-
-
